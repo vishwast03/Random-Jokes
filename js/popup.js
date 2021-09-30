7499e2c67390ecd2a48c6734b65d25b0ccaffc4a
Vishwas Tyagi <vishwast8126@gmail.com>
2021-09-30 18:01:38 +0530

    minor changes

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,10 @@
+/*Storing DOM elements in variables */
 const jokeSect = document.getElementById('joke-text');
 const nextBtn = document.getElementById('next-btn');
 const category = document.getElementById('category');
 const changeBtn = document.getElementById('category-btn');
 
+/*Function to display jokes which are composed of two parts (setup and delivery) */
 function displayTwoPart(data) {
     const setup = `<p>${data.setup}</p>`;
     const delivery = `${setup}<p style="color: green;">${data.delivery}</p>`;
@@ -10,13 +12,17 @@ function displayTwoPart(data) {
     setTimeout(() => {jokeSect.innerHTML = delivery}, 3000);
 }
 
+/*Function to display jokes which are composed of single part (joke) */
 function displaySingle(data) {
     const joke = `<p style="color: green;">${data.joke}</p>`;
     jokeSect.innerHTML = joke;
 }
 
+/*Function to fetch data form jokeapi according to user preferred category */
 function getData() {
-    chrome.storage.local.get(['random-jokes'], function(result) {
+
+    /*getting user preferred category from the local storage */
+    chrome.storage.local.get(['random-jokes'], function(result) { 
         let fetchCategory;
         if(Object.entries(result).length == 0) {
             fetchCategory = "any";
@@ -38,19 +44,22 @@ function getData() {
     });
 }
 
+/*Function to set the category and storing it in local storage */
 function setCategory() {
     chrome.storage.local.set({'random-jokes': category.value}, function() {
-        console.log('value is set to ' +category.value);
+        
     });
 }
 
 getData();
 
+/*Adding event listener to the add buttton */
 nextBtn.addEventListener('click', () => {
     jokeSect.innerHTML = `<div class="loading"></div>`;
     getData();
 });
 
+/*Adding event listener to the change buttton */
 changeBtn.addEventListener('click', () => {
     setCategory();
     nextBtn.click();
